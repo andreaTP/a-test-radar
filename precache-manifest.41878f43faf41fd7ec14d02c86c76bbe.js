74699cc512e968f2e038213de3fe18a35cd1d3f7
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2021-05-20 18:25:59 +0100

    initial commit

---
+++ precache-manifest.41878f43faf41fd7ec14d02c86c76bbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a7ab631fd812f0baaa43076a3cd26e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4145e2bd50ee7880e63d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "324c9f4e12910a0555b7",
+    "url": "/static/js/2.b5d3da8f.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/static/js/2.b5d3da8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a882a6764d2223c4a63e",
+    "url": "/static/js/3.3c97e341.chunk.js"
+  },
+  {
+    "revision": "4145e2bd50ee7880e63d",
+    "url": "/static/js/main.2207ee8e.chunk.js"
+  },
+  {
+    "revision": "b9fba88fbcfcb25fec14",
+    "url": "/static/js/runtime-main.995c8c22.js"
+  }
+]);
